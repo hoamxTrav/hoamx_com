6ee649b2a50eb19c62d4020575e7eb75d1ba6a1c
hoamxTrav <travis@hoamx.com>
2025-11-15 15:31:50 -0600

    Update index.js

--- api/logvisit/index.js
+++ api/logvisit/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
-  const ip = (req.headers["x-forwarded-for"] || req.headers["x-client-ip"] || req.ip || "").split(",")[0].trim();
+  const ipHeader = req.headers["x-forwarded-for"] || req.headers["x-client-ip"] || "";
+  const ip = (ipHeader || req.ip || "").toString().split(",")[0].trim();
   const ts = new Date().toISOString();
-  // TODO: persist to SQL (see submitTrial) or Table Storage if preferred
   context.log(`visit from ${ip} @ ${ts}`);
-  context.res = { status: 200, jsonBody: { ok: true, ip, ts } };
+  context.res = { status: 200, headers: { "Content-Type": "application/json" }, body: { ok: true, ip, ts } };
 };
